34a74316561c0d87c2082ab53dc4697db942506b
mailys-hau <27966914+mailys-hau@users.noreply.github.com>
2022-11-30 15:32:56 +0100

    3D: add static plotting

---
+++ src/threed/__init__.py
@@ -0,0 +1,3 @@
+from threed.animated import animated_3d
+from threed.interactive import interactive_3d
+from threed.static import static_3d
---
+++ src/threed/static.py
@@ -0,0 +1,29 @@
+"""
+3D plot of mitral valve voxels from voxel grids (see `utils/voxel_grid.py`)
+"""
+
+import plotly.express as px
+
+from io import BytesIO
+from PIL import Image
+
+from threed.interactive import interactive_3d
+
+
+
+def static_3d(vinput, vlabels=None, vpreds=None, mode="hmap",
+              title="", show=True, filename=None):
+    fig = interactive_3d(vinput, vlabels, vpreds, mode, title, show=False)
+    extension = filename.suffix.strip('.') if filename else "png"
+    img_str = fig.to_image(format=extension)
+    if show:
+        img = Image.open(BytesIO(img_str))
+        fig = px.imshow(img)
+        fig.update_layout(autosize=False, width=600, height=500,
+                          margin=dict(l=0, t=0, r=0, b=0))
+        fig.update_xaxes(showticklabels=False)
+        fig.update_yaxes(showticklabels=False)
+        fig.show()
+    if filename:
+        with open(filename, "wb") as fd:
+            fd.write(img_str)
