cf0f767249ce84a9fad5a4175bcd90df8f4b6e05
mailys-hau <27966914+mailys-hau@users.noreply.github.com>
2022-12-02 17:25:15 +0100

    4D: add 3D plot through time

---
+++ src/threed/animated.py
@@ -0,0 +1,77 @@
+import plotly.graph_objects as go
+
+from plotly.offline import iplot
+
+from utils import BIN_CMAPS, HEAT_CMAPS
+
+#TODO: 3D "annimation", add slider for frames
+#TODO: gif making for sliced plot (3D would be hard to read)
+
+
+
+def _make_plotly_mesh(voxels, level=None, mask=False, stride=1, **kwargs): #FIXME: Make this part of the VoxelGrid class
+    verts, faces, values = voxels.devoxelize(level, mask, stride)
+    x, y, z = zip(*verts)
+    i, j, k = zip(*faces)
+    intensity = None if "color" in kwargs else values
+    return go.Mesh3d(x=x, y=y, z=z, i=i, j=j, k=k, intensity=intensity, **kwargs)
+
+def _frame_args(duration):
+    return {"frame": {"duration": duration, "redraw": True},
+            "mode": "immediate",
+            "fromcurrent": True,
+            "transition": {"duration": duration, "easing": "linear"}}
+
+
+def animated_3d(vinputs, vlabels=None, vpreds=None, threshold=None,
+                title="", show=True, filename=None):
+    frames = []
+    #TODO: Multithread this
+    for i in range(len(vinputs)):
+        data = [_make_plotly_mesh(vinputs[i], level=50, stride=6, opacity=0.3,
+                                  showscale=False, colorscale="ice", reversescale=True)]
+        if vlabels:
+            for k in vlabels.keys():
+                data.append(_make_plotly_mesh(vlabels[k][i], stride=2,
+                                              color=BIN_CMAPS[k][1]))
+        if vpreds:
+            for k in vpreds.keys():
+                if threshold:
+                    data.append(_make_plotly_mesh(vpreds[k][i].float2bool(threshold), stride=2, color=BIN_CMAPS[k][1]))
+                else:
+                    data.append(_make_plotly_mesh(vpreds[k][i], stride=2, colorscale=HEAT_CMAPS[k]))
+        frames.append(go.Frame(data=data, name=str(i)))
+    layout = {"sliders": [{
+                "len": 0.8,
+                'x': 0.1, 'y': 0,
+                "steps": [{
+                    "args": [[str(i)], _frame_args(0)],
+                    "label": str(i),
+                    "method": "animate",
+                  } for i in range(len(frames))]}],
+              "updatemenus": [{
+                  "buttons": [{"args": [None, _frame_args(200)],
+                               "label": "&#9654;", # Play symbol
+                               "method": "animate"},
+                              {"args": [[None], _frame_args(0)],
+                               "label": "&#9724;", # Pause symbol
+                               "method": "animate"}],
+                  "direction": "left",
+                  "type": "buttons",
+                  'x': 0, 'y': 0
+                }],
+             }
+    #FIXME: fix range to smooth animation
+    fig = go.Figure(data=frames[0].data, frames=frames, layout=layout)
+                    #range_x=[], range_y=[], range_z=[])
+    camera = {"eye": {'x': -0.5, 'y': -1.15, 'z': 0.7},
+              "up": {'x': 0, 'y': 0, 'z': 0.5}}
+    fig.update_layout(scene_camera=camera, title=title, title_y=0.98,
+                      plot_bgcolor="rgb(64,64,64)", autosize=False, width=600,
+                      height=500, margin=dict(l=10, t=30, b=10))
+    if show:
+        iplot(fig)
+    if filename:
+        filename = filename.with_suffix(".html") if filename.suffix != ".html" else filename
+        fig.write_html(filename) #FIXME: Reduce file size
+    return frames
