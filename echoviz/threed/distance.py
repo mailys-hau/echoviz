f984c364617bb612f4b02d776ea0959bec77e6b2
mailys-hau <27966914+mailys-hau@users.noreply.github.com>
2023-07-27 16:51:28 +0200

    3D: add new dist (ASD) plot

--- echoviz/threed/distance.py
+++ echoviz/threed/distance.py
@@ -1,6 +1,5 @@
 import plotly.graph_objects as go
 
-from pathlib import Path
 from plotly.offline import iplot
 from pysdf import SDF
 from warnings import warn
@@ -11,7 +10,8 @@ from echoviz.utils.misc import clean_fname
 
 
 
-def sdf_interactive_3d(vlabels, vpreds, vinputs=None, title='', show=True, filename=None):
+def _dist_interactive_3d(vlabels, vpreds, fdist, vinputs=None,
+                         title='', colorscale=SJET_R, show=True, filename=None):
     """
     """
     # Create fancy figure
@@ -30,10 +30,10 @@ def sdf_interactive_3d(vlabels, vpreds, vinputs=None, title='', show=True, filen
             verts, faces, _ = vpreds[k].devoxelize()
         except RuntimeError:
             warn(f"{k.capitalize()} prediction is null, not plotting it.", RuntimeWarning)
-        sdf_res = sdf(verts)
+        sdf_res = fdist(sdf(verts))
         cmin, cmax = min(min(sdf_res), cmin), max(max(sdf_res), cmax)
         mesh = vpreds[k].make_mesh(verts=verts, faces=faces, values=sdf_res, name=k,
-                                   opacity=0.7, cmin=cmin, cmax=cmax, colorscale=SJET_R
+                                   opacity=0.7, cmin=cmin, cmax=cmax, colorscale=colorscale,
                                    contour={"color": "lightgrey", "show": True, "width": 10})
         fig.add_trace(mesh)
     # Homogenize SDF colorscale
@@ -45,3 +45,12 @@ def sdf_interactive_3d(vlabels, vpreds, vinputs=None, title='', show=True, filen
         #FIXME: Diminish file size by removing js modules
         fig.write_html(filename)
     return fig
+
+
+def sdf_interactive_3d(vlabels, vpreds, vinputs=None, title='', show=True, filename=None):
+    return _dist_interactive_3d(vlabels, vpreds, lambda x: x, vinputs=vinputs,
+                                title=title, show=show, filename=filename)
+
+def asd_interactive_3d(vlabels, vpreds, vinputs=None, title='', show=True, filename=None):
+    return _dist_interactive_3d(vlabels, vpreds, lambda x: abs(x), vinputs=vinputs,
+                                title=title, colorscale=SJET, show=show, filename=filename)
