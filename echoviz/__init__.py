a9692062e3c3db10dacddd718f1d9a6d9cc41e11
mailys-hau <27966914+mailys-hau@users.noreply.github.com>
2023-07-28 14:37:56 +0200

    3D: fix: imports

--- echoviz/__init__.py
+++ echoviz/__init__.py
@@ -1,5 +1,5 @@
 from echoviz.threed import (animated_3d, sdf_animated_3d, asd_animated_3d,
-                            interactive_3d, sdf_interactive_3d, adf_interactive_3d
+                            interactive_3d, sdf_interactive_3d, asd_interactive_3d,
                             static_3d, sdf_static_3d, asd_static_3d)
 from echoviz.twod import plot_slice, sliced_sequence, sliced_volume
 from echoviz.utils import VoxelGrid
